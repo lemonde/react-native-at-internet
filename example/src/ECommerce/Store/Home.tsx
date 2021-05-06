5c30f6e48dfdc49c894d09936928453b1f6a62b7
Rodolphe Renaux <rrenaux@idobs.net>
2021-05-06 10:28:44 +0200

    chore(example): totally rework app to better integrate use cases

---
+++ example/src/ECommerce/Store/Home.tsx
@@ -0,0 +1,73 @@
+import { useFocusEffect, useNavigation } from '@react-navigation/core';
+import * as React from 'react';
+import { View, TouchableOpacity, FlatList } from 'react-native';
+import { Text, Card } from 'react-native-elements';
+import AtInternet, {
+  ECommerceProduct,
+} from '@lemonde/react-native-at-internet';
+import type { Product } from '../../constants';
+import { DATA } from '../../constants';
+
+const Item = (item: Product) => {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('Product', { item });
+      }}
+    >
+      <Card>
+        <Card.Title>{item.title}</Card.Title>
+        <Card.Image
+          source={{
+            uri: item.image,
+          }}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            alignContent: 'stretch',
+            alignItems: 'stretch',
+            justifyContent: 'space-between',
+            flex: 1,
+            width: '100%',
+          }}
+        >
+          <Text>{item.stock} left in stock</Text>
+          <Text>{`${item.price} ${item.currency}`}</Text>
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
+export default function Home() {
+  const renderItem = ({ item }: { item: Product }) => <Item {...item} />;
+
+  useFocusEffect(
+    React.useCallback(() => {
+      AtInternet.screen({ name: 'Home' });
+      AtInternet.SalesInsights.Products.display(
+        ...DATA.map((item) => {
+          const product: ECommerceProduct = {
+            id: item.id,
+            $: item.title,
+            pricetaxincluded: item.price,
+            stock: item.stock > 0,
+          };
+
+          return product;
+        })
+      );
+    }, [])
+  );
+
+  return (
+    <FlatList
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+}
